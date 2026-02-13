fa1eef337912f95ceda800b84d862df79e9a2097
Vasilis Theodosiadis <vasilist@thevtool.com>
2026-02-13 12:49:32 +0000

    apb transaction delay

--- testbench/agent/apb_transacation.sv
+++ testbench/agent/apb_transacation.sv
@@ -6,12 +6,13 @@ class apb_transaction extends uvm_sequence_item;
     rand logic write;
     rand data_t data; 
     rand address_t addr;
+    rand logic delay;
 
     logic ready;
     logic slv_error;
 
     constraint c_addr {addr inside{[0:4]};}
-
+    constraint c_delay {delay inside{[0:50]};}
 
 function new(string name ="");
     super.new(name);
